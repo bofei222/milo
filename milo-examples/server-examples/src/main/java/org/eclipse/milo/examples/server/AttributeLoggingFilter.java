be4459f0d52590e327bf0a832265449fed406eeb
bofei <bofei222@163.com>
2023-07-03 15:53:36 +0800

    注释log

--- milo-examples/server-examples/src/main/java/org/eclipse/milo/examples/server/AttributeLoggingFilter.java
+++ milo-examples/server-examples/src/main/java/org/eclipse/milo/examples/server/AttributeLoggingFilter.java
@@ -10,14 +10,14 @@
 
 package org.eclipse.milo.examples.server;
 
-import java.util.function.Predicate;
-
 import org.eclipse.milo.opcua.sdk.server.nodes.filters.AttributeFilter;
 import org.eclipse.milo.opcua.sdk.server.nodes.filters.AttributeFilterContext.GetAttributeContext;
 import org.eclipse.milo.opcua.sdk.server.nodes.filters.AttributeFilterContext.SetAttributeContext;
 import org.eclipse.milo.opcua.stack.core.AttributeId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.function.Predicate;
 
 public class AttributeLoggingFilter implements AttributeFilter {
 
@@ -39,10 +39,10 @@ public class AttributeLoggingFilter implements AttributeFilter {
 
         // only log external reads
         if (attributePredicate.test(attributeId) && ctx.getSession().isPresent()) {
-            logger.info(
-                "get nodeId={} attributeId={} value={}",
-                ctx.getNode().getNodeId(), attributeId, value
-            );
+//            logger.info(
+//                "get nodeId={} attributeId={} value={}",
+//                ctx.getNode().getNodeId(), attributeId, value
+//            );
         }
 
         return value;
